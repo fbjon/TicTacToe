c2c61f428039fa97907ee8a3c72b93fb1bc927fb
Staffan Strömsholm <staffan.stromsholm@gmail.com>
2020-02-20 07:04:30 +0200

    basic single player functionality works

--- index.js
+++ index.js
@@ -8,79 +8,106 @@ var td6 = document.getElementById('23')
 var td7 = document.getElementById('31')
 var td8 = document.getElementById('32')
 var td9 = document.getElementById('33')
+var header = document.querySelector('h1');
+var gameOver = false;
+var array = [];
 
-
-allTds.forEach(function (td) {
-    td.addEventListener('click', function () {
-        this.classList.add('X');
-        setTimeout(add0, 500);
-        gameOver();
-    })
-})
-
-function add0() {
-    let emptySlots = document.querySelectorAll("td:not(.X):not(.O)")
-    let i = Math.floor(Math.random() * (emptySlots.length));
-    emptySlots[i].classList.add('O');
-    gameOver();
-}
-
-function gameOver() {
-    if (td1.classList.contains('X') && td2.classList.contains('X') && td3.classList.contains('X')) {
-        console.log("X wins!")
-        removeEventlistener()
-    } else if (td4.classList.contains('X') && td5.classList.contains('X') && td6.classList.contains('X')) {
-        console.log("X wins!")
-        removeEventlistener()
-    } else if (td7.classList.contains('X') && td8.classList.contains('X') && td9.classList.contains('X')) {
-        console.log("X wins!")
-        removeEventlistener()
-    } else if (td1.classList.contains('X') && td4.classList.contains('X') && td7.classList.contains('X')) {
-        console.log("X wins!")
-        removeEventlistener()
-    } else if (td2.classList.contains('X') && td5.classList.contains('X') && td8.classList.contains('X')) {
-        console.log("X wins!")
-        removeEventlistener()
-    } else if (td3.classList.contains('X') && td6.classList.contains('X') && td9.classList.contains('X')) {
-        console.log("X wins!")
-        removeEventlistener()
-    } else if (td1.classList.contains('X') && td5.classList.contains('X') && td9.classList.contains('X')) {
-        console.log("X wins!")
-        removeEventlistener()
-    } else if (td3.classList.contains('X') && td5.classList.contains('X') && td7.classList.contains('X')) {
-        console.log("X wins!")
-        removeEventlistener()
-    } else if (td1.classList.contains('O') && td2.classList.contains('O') && td3.classList.contains('O')) {
-        console.log("O wins!")
-        removeEventlistener()
-    } else if (td4.classList.contains('O') && td5.classList.contains('O') && td6.classList.contains('O')) {
-        console.log("O wins!")
-        removeEventlistener()
-    } else if (td7.classList.contains('O') && td8.classList.contains('O') && td9.classList.contains('O')) {
-        console.log("O wins!")
-        removeEventlistener()
-    } else if (td1.classList.contains('O') && td4.classList.contains('O') && td7.classList.contains('O')) {
-        console.log("O wins!")
-        removeEventlistener()
-    } else if (td2.classList.contains('O') && td5.classList.contains('O') && td8.classList.contains('O')) {
-        console.log("O wins!")
-        removeEventlistener()
-    } else if (td3.classList.contains('O') && td6.classList.contains('O') && td9.classList.contains('O')) {
-        console.log("O wins!")
-        removeEventlistener()
-    } else if (td1.classList.contains('O') && td5.classList.contains('O') && td9.classList.contains('O')) {
-        console.log("O wins!")
-        removeEventlistener()
-    } else if (td3.classList.contains('O') && td5.classList.contains('O') && td7.classList.contains('O')) {
-        console.log("O wins!")
+window.onload = function () {
+    let i = Math.random();
+    console.log(i);
+    if (i > 0.5) {
+        addO();
+    } else {
+        addEventlisters();
     }
 }
 
-function removeEventlistener(){
+function addX() {
+    if (!this.classList.contains('X') && !this.classList.contains('O')) {
+        array.push('X');
+        this.classList.add('X');
+        checkGameOver();
+        removeEventListeners()
+        if (gameOver === false) {
+            setTimeout(addO, 500);
+        }
+    }
+}
+
+function addO() {
+    if (array.length < 9) {
+        array.push('O');
+        let emptySlots = document.querySelectorAll("td:not(.X):not(.O)")
+        let i = Math.floor(Math.random() * (emptySlots.length));
+        emptySlots[i].classList.add('O');
+        addEventlisters();
+        checkGameOver();
+    }
+}
+
+function checkGameOver() {
+    if (td1.classList.contains('X') && td2.classList.contains('X') && td3.classList.contains('X')) {
+        endGameX();
+    } else if (td4.classList.contains('X') && td5.classList.contains('X') && td6.classList.contains('X')) {
+        endGameX();
+    } else if (td7.classList.contains('X') && td8.classList.contains('X') && td9.classList.contains('X')) {
+        endGameX();
+    } else if (td1.classList.contains('X') && td4.classList.contains('X') && td7.classList.contains('X')) {
+        endGameX();
+    } else if (td2.classList.contains('X') && td5.classList.contains('X') && td8.classList.contains('X')) {
+        endGameX();
+    } else if (td3.classList.contains('X') && td6.classList.contains('X') && td9.classList.contains('X')) {
+        endGameX();
+    } else if (td1.classList.contains('X') && td5.classList.contains('X') && td9.classList.contains('X')) {
+        endGameX();
+    } else if (td3.classList.contains('X') && td5.classList.contains('X') && td7.classList.contains('X')) {
+        endGameX();
+    } else if (td1.classList.contains('O') && td2.classList.contains('O') && td3.classList.contains('O')) {
+        endGameO()
+    } else if (td4.classList.contains('O') && td5.classList.contains('O') && td6.classList.contains('O')) {
+        endGameO()
+    } else if (td7.classList.contains('O') && td8.classList.contains('O') && td9.classList.contains('O')) {
+        endGameO()
+    } else if (td1.classList.contains('O') && td4.classList.contains('O') && td7.classList.contains('O')) {
+        endGameO()
+    } else if (td2.classList.contains('O') && td5.classList.contains('O') && td8.classList.contains('O')) {
+        endGameO()
+    } else if (td3.classList.contains('O') && td6.classList.contains('O') && td9.classList.contains('O')) {
+        endGameO()
+    } else if (td1.classList.contains('O') && td5.classList.contains('O') && td9.classList.contains('O')) {
+        endGameO()
+    } else if (td3.classList.contains('O') && td5.classList.contains('O') && td7.classList.contains('O')) {
+        endGameO()
+    } else if (array.length === 9) {
+        endGame();
+    }
+}
+
+function endGameX() {
+    header.innerHTML = "<img src='/pics/X.jpg' width='42' height='42' align='center'> wins!"
+    gameOver = true;
+    removeEventListeners();
+}
+
+function endGameO() {
+    header.innerHTML = "<img src='/pics/O.png' width='42' height='42' align='center'> wins!"
+    gameOver = true;
+    removeEventListeners();
+}
+
+function endGame() {
+    removeEventListeners();
+    header.innerHTML = "Game Over!"
+}
+
+function addEventlisters() {
     allTds.forEach(function (td) {
-        td.removeEventListener('click', function () {
-            this.classList.add('X')
-        
-        })
+        td.addEventListener('click', addX)
+    })
+}
+
+function removeEventListeners() {
+    allTds.forEach(function (td) {
+        td.removeEventListener('click', addX)
     })
 }
